6f006d2b02d3dcb5efd2855b61ee52955347ab8d
sorrel012 <wain1719@gmail.com>
2023-08-16 14:55:47 +0900

    chore: Reset git

---
+++ src/main/java/com/postcard/toyou/ToyouApplication.java
@@ -0,0 +1,13 @@
+package com.postcard.toyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postcard/toyou/ToyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postcard.toyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
