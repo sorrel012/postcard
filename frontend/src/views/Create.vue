d40ed60bdc0af93f95d4a0817b25f9e58bdcb969
sorrel012 <wain1719@gmail.com>
2023-08-17 15:57:56 +0900

    feat: Design creation page

---
+++ frontend/src/views/Create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ name }}의 롤링페이퍼 💌</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'create',
+    }
+  },
+  created() {
+    this.name = sessionStorage.getItem('name');
+  }
+}
+</script>
+
+<style></style>
