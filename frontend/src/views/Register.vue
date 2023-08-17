a420ca81ccfee75db13bbfa59c155827a9c91335
sorrel012 <wain1719@gmail.com>
2023-08-17 10:13:12 +0900

    style: Change form's width

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -8,28 +8,28 @@
                 <h1 class="h2 text-center fw-bold title">개인 회원가입</h1>
                 <p class="fs-6 text-center mb-4">회원가입 후 다양한 서비스를 이용하세요.</p>
                 
-                <form class="w-75" @submit="register" id="formregister">               
+                <form class="w-100" @submit="register" id="formregister">               
                     <div class="mb-1">아이디</div>
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="userid" placeholder="name@example.com" name="m_id" v-model="id"> 
+                        <input type="text" class="form-control" id="userid" placeholder="name@example.com" name="m_id" v-model="userinfo.id"> 
                         <label for="floatingInput">6~12자 이내로 입력해 주세요</label>
                     </div>
                         
                     <div class="mb-1">비밀번호</div>
                         <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="password" placeholder="Password" name="m_pw" required v-model="pw"> 
+                        <input type="password" class="form-control" id="password" placeholder="Password" name="m_pw" required v-model="userinfo.pw"> 
                         <label for="floatingPassword">8~12자 이내로 입력해 주세요 (알파벳, 숫자 필수)</label>
                     </div>
 
                     <div class="mb-1">이름</div>
                         <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="name" placeholder="name" name="m_name" required v-model="name"> 
+                        <input type="text" class="form-control" id="name" placeholder="name" name="m_name" required v-model="userinfo.name"> 
                         <label for="floatingPassword" >이름을 입력해 주세요</label>
                     </div>
                     
                     <div class="mb-1">연락처</div>
                         <div class="form-floating">
-                        <input type="text" class="form-control" id="tel" placeholder="tel" name="m_tel" required v-model="tel"> 
+                        <input type="text" class="form-control" id="tel" placeholder="tel" name="m_tel" required v-model="userinfo.tel"> 
                         <label for="floatingPassword">"-" 없이 입력해 주세요</label>
                     </div>
                         
