c0cb2d9fd77f06d52838934e3b1527814812be33
sorrel012 <wain1719@gmail.com>
2023-09-19 13:44:20 +0900

    fix: Redirect to the main page upon failed login

---
+++ src/main/java/com/postcard/toyou/service/SnsDisconEnum.java
@@ -0,0 +1,32 @@
+package com.postcard.toyou.service;
+
+public enum SnsDisconEnum {
+    KAKAO("카카오", "https://kapi.kakao.com/v1/user/unlink"),
+    NAVER("네이버", "https://openapi.naver.com/v1/nid/me"),
+    GOOGLE("구글", "https://www.googleapis.com/userinfo/v2/me");
+
+    private final String name;
+    private final String uri;
+
+    SnsDisconEnum(String name, String uri) {
+        this.name = name;
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public static SnsDisconEnum toUpper(String btnType) {
+        try {
+            return SnsDisconEnum.valueOf(btnType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid btnType: " + btnType);
+        }
+    }
+
+}
