e861500aa993ef939c32d1196869389fcddfc3a8
sorrel012 <wain1719@gmail.com>
2023-10-12 15:29:15 +0900

    refactor: Extract decoding & parsing file name method

--- src/main/java/com/postcard/toyou/service/S3FileUploadService.java
+++ src/main/java/com/postcard/toyou/service/S3FileUploadService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +22,14 @@ public class S3FileUploadService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    /**
+     * aws에 파일을 저장하는 메소드
+     * 
+     * @param multipartFile
+     * @param newFileName
+     * @return
+     * @throws IOException
+     */
     public String saveFile(MultipartFile multipartFile, String newFileName) throws IOException {
 
         ObjectMetadata metadata = new ObjectMetadata();
@@ -31,25 +40,53 @@ public class S3FileUploadService {
         return amazonS3.getUrl(bucket, newFileName).toString();
     }
 
+    /**
+     * aws에서 파일을 삭제하는 메소드
+     * 
+     * @param deletedImg
+     */
     public void deleteImage(List<String> deletedImg)  {
 
         for (String img : deletedImg) {
 
-            String decodedUrl = "";
-            
-            //URL 디코딩
-            try {
-                decodedUrl = URLDecoder.decode(img, "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            String awsUrl = "https://postcard17.s3.ap-northeast-2.amazonaws.com/";
-            img = decodedUrl.substring(awsUrl.length());
+            String decodedUrl = decodeUrl(img);
+            img = extractFileName(decodedUrl);
 
             amazonS3.deleteObject(bucket, img);
         }
 
+    }
+
+    /**
+     * aws 파일 url을 디코딩하는 메소드
+     *
+     * @param imgUrl : aws 파일 url
+     * @return
+     */
+    public String decodeUrl(String imgUrl) {
+
+        //URL 디코딩
+        String decodedUrl = "";
+        try {
+            decodedUrl = URLDecoder.decode(imgUrl, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return decodedUrl;
+    }
+
+    /**
+     * url에서 파일명을 추출하는 메소드
+     * 
+     * @param decodedUrl : 디코딩된 파일 url
+     * @return
+     */
+    public String extractFileName(String decodedUrl) {
+        String awsUrl = "https://postcard17.s3.ap-northeast-2.amazonaws.com/";
+        String fileName = decodedUrl.substring(awsUrl.length());
+
+        return fileName;
     }
 
 }
